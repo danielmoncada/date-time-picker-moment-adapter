9118057f53984900cdb4600623c55a29de7ac6e8
Daniel Moncada <daniel.g.moncada@gmail.com>
2022-01-20 00:00:28 -0800

    Update API

--- projects/picker/src/public_api.ts
+++ projects/picker/src/public_api.ts
@@ -1,1 +1,7 @@
 export { OwlMomentDateTimeModule } from './lib/moment-adapter/moment-date-time.module';
+
+export { 
+    OwlMomentDateTimeAdapterOptions, 
+    OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS,
+    OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS_FACTORY
+ } from './lib/moment-adapter/moment-date-time-adapter.class';
