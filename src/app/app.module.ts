491e2298896bba94bb585f702481816c2665b1f1
Floris van der Haar <florisvanderhaar@gmail.com>
2024-12-19 14:32:03 +0100

    NG19 Standalone component migration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import {
-  Options,
-  OptionsTokens,
-  OWL_DATE_TIME_FORMATS,
-  OWL_DATE_TIME_LOCALE,
-  OwlDateTimeModule,
-  OwlNativeDateTimeModule,
-} from '@danielmoncada/angular-datetime-picker';
-
-import { AppComponent } from './app.component';
-import { OwlMomentDateTimeModule } from '../../projects/picker/src/public_api';
-
 export const MY_MOMENT_FORMATS = {
   parseInput: 'l LT',
   fullPickerInput: 'l LT',
@@ -24,29 +7,3 @@ export const MY_MOMENT_FORMATS = {
   dateA11yLabel: 'LL',
   monthYearA11yLabel: 'MMMM YYYY',
 };
-
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    OwlDateTimeModule,
-    OwlNativeDateTimeModule,
-    OwlMomentDateTimeModule,
-  ],
-  providers: [
-    { provide: OWL_DATE_TIME_LOCALE, useValue: 'en-US' },
-    {
-      provide: OptionsTokens.multiYear,
-      useFactory: () =>
-        ({
-          yearRows: 5,
-          yearsPerRow: 3,
-        }) as Options['multiYear'],
-    },
-    { provide: OWL_DATE_TIME_FORMATS, useValue: MY_MOMENT_FORMATS },
-  ],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
